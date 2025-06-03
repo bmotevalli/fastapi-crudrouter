643c2b290d75e60186078f96e028aa8f72b49ac1
bmotevalli <b.motevalli@gmail.com>
2025-06-03 14:25:40 +0800

    fixed to make it compatible with pydantic v2.

--- fastapi_crudrouter/core/_utils.py
+++ fastapi_crudrouter/core/_utils.py
@@ -14,7 +14,7 @@ class AttrDict(dict):  # type: ignore
 
 def get_pk_type(schema: Type[PYDANTIC_SCHEMA], pk_field: str) -> Any:
     try:
-        return schema.__fields__[pk_field].type_
+        return schema.model_fields[pk_field].annotation
     except KeyError:
         return int
 
